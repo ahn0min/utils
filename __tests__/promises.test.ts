598c3f9e30d420c98396255e72e89d9348dc4c8d
0min <ahn0min95@gmail.com>
2023-09-10 21:06:11 +0900

    axios와 테스트해보기 위한 커밋

--- __tests__/promises.test.ts
+++ __tests__/promises.test.ts
@@ -1,6 +1,7 @@
 const {
   sleep,
   timeout,
+  retry,
   TIMEOUT_ERROR_MESSAGE,
   RESPONSE_ERROR_MESSAGE,
 } = require("@/promises");
@@ -10,6 +11,8 @@ describe("sleep", () => {
     const start: Date = new Date();
     await sleep(500);
     const end: Date = new Date();
+
+    // TODO: consider check time to time difference utils
     const diff: number = end.getTime() - start.getTime();
     expect(diff).toBeGreaterThanOrEqual(499);
   });
@@ -67,3 +70,96 @@ describe("timeout", () => {
     ).rejects.toThrow(new Error(TIMEOUT_ERROR_MESSAGE));
   });
 });
+
+// test를 먼저 짜보자.
+
+describe("retry", () => {
+  // const [MAXIMUM_RETRY, INTEGER] = [2, 150];
+
+  const MAXIMUM_RETRY = 10;
+
+  const INTEGER = {
+    PASS: 100,
+    BASE: 200,
+    // OVER: 200,
+    OVER: 2000,
+  } as const;
+  // const PROMISE_END_TIME = {
+  //   OVER: INTEGER + 50,
+  //   PASS: INTEGER - 50,
+  // };
+
+  const RESPONSE_DATA = "return data";
+
+  test("retry가 일어난걸 체크", async () => {
+    // 첫케이스
+    const start = new Date();
+    await retry(
+      () => mockResolvedPromise(RESPONSE_DATA, INTEGER.OVER),
+      MAXIMUM_RETRY,
+      INTEGER.BASE
+    );
+    const end = new Date();
+    const difference = end.getTime() - start.getTime();
+
+    // await expect(difference).toBeGreaterThanOrEqual(INTEGER.BASE);
+    expect(difference).toBeLessThanOrEqual(INTEGER.OVER * 2);
+
+    // 두번째 케이스
+  });
+
+  /*
+  test("retry no Because only resolve", async () => {
+    const start = new Date();
+    const result = await retry(
+      () => mockResolvedPromise(RESPONSE_DATA, PROMISE_END_TIME.PASS),
+      MAXIMUM_RETRY,
+      INTEGER
+    );
+    const end = new Date();
+
+    const diffrence = end.getTime() - start.getTime();
+    expect(diffrence).toBeLessThanOrEqual(INTEGER);
+  });
+
+  */
+
+  // test(`retryed only once Because the first promise is resolved when the second promise is pending.`, async () => {
+  //   const start = new Date();
+  //   const result = await retry(
+  //     () => mockResolvedPromise(RESPONSE_DATA, PROMISE_END_TIME.OVER),
+  //     MAXIMUM_RETRY,
+  //     INTEGER
+  //   );
+  //   const end = new Date();
+
+  //   const diffrence = end.getTime() - start.getTime();
+
+  //   expect(result).toBe(RESPONSE_DATA);
+  //   // to be greater than or equal why? end time is all retry after
+  //   expect(diffrence).toBeGreaterThanOrEqual(PROMISE_END_TIME.OVER);
+  // });
+
+  // test(`어떤거 체크할까 2번 pending일때 1번이 resolved가 되었을 떄 더이상 호출을 하지 않는지`, async () => {
+  //   const start = new Date();
+  //   const result = await retry(
+  //     () => mockResolvedPromise(RESPONSE_DATA, PROMISE_END_TIME.OVER * 5),
+  //     2,
+  //     INTEGER
+  //   );
+  //   const end = new Date();
+
+  //   const diffrence = end.getTime() - start.getTime();
+
+  //   // count 확인해보자.
+  //   expect(result).toBe([RESPONSE_DATA, 2]);
+  //   expect(diffrence).toBeLessThanOrEqual(INTEGER * MAXIMUM_RETRY);
+  //   expect(diffrence).toBeGreaterThanOrEqual(PROMISE_END_TIME.OVER);
+
+  //   // 왜 throw error를 하는걸까?
+  // });
+
+  // test("0번인 경우");
+});
+
+// pending인지 fullfilled인지 확인하는것도 있으면 좋겠다.
--- src/promises.ts
+++ src/promises.ts
@@ -37,4 +37,144 @@ function timeout<T>(promise: Promise<T>, time = 8000) {
   });
 }
 
-export { sleep, timeout, TIMEOUT_ERROR_MESSAGE, RESPONSE_ERROR_MESSAGE };
+/**
+ *  description
+ * @template T
+ * @param {Promise<T>} promiseFunction
+ * @param {number} maximumRetry
+ * @param {number} retryInterval
+ *
+ */
+
+/*
+✅ 이전에 작성항 코드
+async function retry<T>(
+  promiseFunction: () => Promise<T>,
+  maximumRetry: number,
+  retryInterval: number
+) {
+  let retryCount = 0;
+
+  function isMaximum() {
+    return retryCount >= maximumRetry;
+  }
+
+  // 한번만 retry를 하네 지금보니까.
+  // for문같은걸 돌려줘야하네.
+
+  // for(let i = 0; i < maximumRetry; i++) {
+  //   try {
+
+  //   } catch(err) {}
+  // }
+
+  const retryTimeoutId = setTimeout(() => {
+    if (isMaximum()) {
+      throw new Error("isMaximum");
+    }
+
+    console.log(`retry start count is ${retryCount}`);
+    promiseFunction();
+    retryCount++;
+  }, retryInterval);
+
+  try {
+    const response = await promiseFunction();
+    clearTimeout(retryTimeoutId);
+    // return response;
+    console.log("retryCount", retryCount);
+    return [response, retryCount];
+  } catch (err) {
+    if (isMaximum()) {
+      throw new Error(
+        "Timeout Error: you promise function call is over maximum retry"
+      );
+    }
+    // 그렇지 않은 경우에는 걍 여기서 끝내버린다. 어차피 retry에서 throw 해줄거기때문에
+  }
+}
+
+*/
+
+// async function retry<T>(
+//   promiseFunction: () => Promise<T>,
+//   maximumRetry: number,
+//   retryInterval: number
+// ) {
+//   const setTimeoutIds = [];
+
+//   for (let retryCount = 0; retryCount < maximumRetry; retryCount++) {
+//     const timeoutId = setTimeout(() => {}, retryInterval * retryCount + 1);
+//     setTimeoutIds.push();
+//   }
+
+//   let retryCount = 0;
+
+//   function isMaximum() {
+//     return retryCount >= maximumRetry;
+//   }
+
+//   // const retryTimeoutId = setTimeout(() => {
+//   //   if (isMaximum()) {
+//   //     throw new Error("isMaximum");
+//   //   }
+
+//   //   console.log(`retry start count is ${retryCount}`);
+//   //   promiseFunction();
+//   //   retryCount++;
+//   // }, retryInterval);
+
+//   // try {
+//   //   const response = await promiseFunction();
+//   //   clearTimeout(retryTimeoutId);
+//   //   // return response;
+//   //   console.log("retryCount", retryCount);
+//   //   return [response, retryCount];
+//   // } catch (err) {
+//   //   if (isMaximum()) {
+//   //     throw new Error(
+//   //       "Timeout Error: you promise function call is over maximum retry"
+//   //     );
+//   //   }
+//   //   // 그렇지 않은 경우에는 걍 여기서 끝내버린다. 어차피 retry에서 throw 해줄거기때문에
+//   // }
+// }
+
+// 반환하지 않아도 될것같은데?
+
+async function retry<T>(
+  promiseFunction: () => Promise<T>,
+  maximumRetry: number = 5,
+  retryInterval: number = 3000
+): Promise<any> {
+  const retryCount = 0;
+  const shouldRetry = retryCount <= maximumRetry;
+
+  console.log(`
+  maximumRetry: ${maximumRetry}
+  `);
+
+  if (shouldRetry) {
+    // 얘는 넘기지 않고 몇초간 기다린다.
+    try {
+      const result = promiseFunction();
+      await timeout(result, retryInterval);
+
+      return result;
+    } catch (err) {
+      // 타임아웃에서 throw error를 했다면
+      return retry(promiseFunction, maximumRetry - 1, retryInterval);
+    }
+
+    // console.error(difference);
+    // console.log(start.getSeconds(), end.getSeconds());
+
+    // 아직까지 이전 그게 끝나지 않았다면 retry해준다.
+  }
+
+  return Promise.reject("retry error");
+}
+
+// function run();
+
+export { sleep, timeout, retry, TIMEOUT_ERROR_MESSAGE, RESPONSE_ERROR_MESSAGE };
